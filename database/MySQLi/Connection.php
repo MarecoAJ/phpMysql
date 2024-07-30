7ec2789979d679e28d8b631fdf2ff48154072bfb
Alejandro J Mareco <a.j.mareco.cit@gmail.com>
2024-07-30 20:31:16 -0300

    claseando la conexion

--- database/MySQLi/Connection.php
+++ database/MySQLi/Connection.php
@@ -1,23 +1,49 @@
 <?php
 
-$server ="localhost";
-$database = "finanzas_php";
-$username = "root";
-$password = "";
 
-//$mysqli = mysqli_connect($server, $username, $password, $database);
+class Connection {
+    private static $instancia;
+    private $connection;
 
-//POO
-$mysqli = new mysqli($server, $username, $password, $database);
-/*
-if(!$mysqli){
-     die("Fallo de conexion". mysqli_connect_error());  
+    private function __construct(){
+        $this->crear_conexion();
+    }
+
+    public static function getIntancia(){
+        if(!self::$instancia instanceof self){
+            self::$instancia = new self();
+        }
+        
+        return self::$instancia;
+    }
+
+    public function get_db_instancia(){
+        return $this->connection;
+    }
+
+    private function crear_conexion(){
+        
+        $server ="localhost";
+        $database = "finanzas_php";
+        $username = "root";
+        $password = "";
+        //$mysqli = mysqli_connect($server, $username, $password, $database);
+        //POO
+        $mysqli = new mysqli($server, $username, $password, $database);
+        /*
+        if(!$mysqli){
+            die("Fallo de conexion". mysqli_connect_error());  
+        }
+        */
+        if($mysqli->connect_errno){
+            die("Fallo de conexion: {$mysqli->connect_error}"); 
+        }
+
+        $setnames = $mysqli->prepare("SET NAMES 'utf8'");
+        $setnames->execute();
+
+        $this->connection = $mysqli;
+    }
+
 }
-*/
-if($mysqli->connect_errno){
-    die("Fallo de conexion: {$mysqli->connect_error}"); 
-}
-
-$setnames = $mysqli->prepare("SET NAMES 'utf8'");
-$setnames->execute();
 ?>
