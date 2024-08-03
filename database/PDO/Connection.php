8de210f9dd40210c7d910e5d5cfd8886359f0ed4
Alejandro J Mareco <a.j.mareco.cit@gmail.com>
2024-08-02 23:02:55 -0300

    claseando PDO

--- database/PDO/Connection.php
+++ database/PDO/Connection.php
@@ -1,12 +1,40 @@
 <?php
 
-$server ="localhost";
-$database = "finanzas_php";
-$username = "root";
-$password = "";
+namespace Database\PDO;
 
-$connection = new PDO("mysql:host=$server;dbname=$database", $username, $password);
-$setnames = $connection->prepare("SET NAMES 'utf8'");
-$setnames->execute();
-var_dump($setnames);
+class Connection {
+
+    private static $instancia;
+    private $connection;
+
+    private function __construct(){
+        $this->crear_conexion();
+    }
+
+    public static function getInstacia(){
+        if(!self::$instancia instanceof self){
+            self::$instancia = new self();
+        }
+        return self::$instancia;
+    }
+
+    public function get_db_instancia(){
+        return $this->connection;
+    }
+
+    private function crear_conexion(){
+        $server ="localhost";
+        $database = "finanzas_php";
+        $username = "root";
+        $password = "";
+
+        $connection = new \PDO("mysql:host=$server;dbname=$database", $username, $password);
+        $setnames = $connection->prepare("SET NAMES 'utf8'");
+        $setnames->execute();
+        var_dump($setnames);
+
+    }
+
+}
+
 ?>
