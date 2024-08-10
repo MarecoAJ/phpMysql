2fa7f788f95b5157fc4b207439237ec5e476a447
Alejandro J Mareco <a.j.mareco.cit@gmail.com>
2024-08-10 11:15:11 -0300

    mejora de seguridad

--- app/controladores/ControladorSalidas.php
+++ app/controladores/ControladorSalidas.php
@@ -16,8 +16,14 @@ class ControladorSalidas {
             "INSERT INTO salidas (tipos_pagos, tipos, fecha_pago, monto, descripcion) 
             VALUES(:tipos_pagos, :tipos, :fecha_pago, :monto, :descripcion);"
         );
-        $statement->execute($datos);
-        
+
+        $statement->bindValue(":tipos_pagos", $datos["tipos_pagos"]);
+        $statement->bindValue(":tipos", $datos["tipos"]);
+        $statement->bindValue(":fecha_pago", $datos["fecha_pago"]);
+        $statement->bindValue(":monto", $datos["monto"]);
+        $statement->bindValue(":descripcion", $datos["descripcion"]);
+        $statement->execute();
+         
     }
 
     public function mostrarCrearSalida(){}
